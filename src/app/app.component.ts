4b808a2f818a5c757bb42108fda532f8d0688cb4
Sviatoslav Kovalev <your_email@youremail.com>
2021-06-18 10:26:49 +0300

    Thursday

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,23 @@ import {HttpClient} from '@angular/common/http'
 export class AppComponent {
  userName: string = ''
  response: any
-
+pageNum: number = 1
  constructor(private http: HttpClient){
 
  }
  search(){
-   this.http.get("https://cors.bridged.cc/https://api.itbook.store/1.0/search/" + this.userName)
+   this.http.get("https://cors.bridged.cc/https://api.itbook.store/1.0/search/" + this.userName +'/' + this.pageNum)
    .subscribe((response)=>{
      this.response = response ? response : []
      console.log(this.response)
    })
+ }
+ addBooks(){
+  this.pageNum = this.pageNum + 1
+  this.http.get("https://cors.bridged.cc/https://api.itbook.store/1.0/search/" + this.userName +'/' + this.pageNum)
+  .subscribe((response)=>{
+    this.response.books.push(response.books)
+    console.log(this.response)
+  })
  }
 }
